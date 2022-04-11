689abbc162323f192b71e6bb45cf2a90b8a90d8d
Petro Halimurka <p.halimurka@gmail.com>
2022-04-11 17:34:29 +0300

    Add flight params, finish search form partial

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,11 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     @airport_options = Airport.all.map { | a | [a.name, a.id] }
-    @date_options = Flight.all.map {| f| f.start_datetime}
+    @day_options = Flight.all.map { |flight| flight.start_datetime.to_date.day }
+    @month_options = Flight.all.map { |flight| flight.start_datetime.to_date.strftime("%B") }
+    @year_options = Flight.all.map { |flight| flight.start_datetime.to_date.year }
+    @selected_date = [params[:day], params[:month], params[:year]].join(" ").to_date
+    @available_flights = Flight.where(departure_airport_id: params[:departure_airport_id], arrival_airport_id: params[:arrival_airport_id], start_datetime: @selected_date)
   end
+
 end
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,5 @@
 module FlightsHelper
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :flight_duration, :start_datetime)
+  end
 end
